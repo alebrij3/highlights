c9b1de8861a0980e9d10584c43d7655a1efe3f0f
Alex <ajf@fastmail.com>
2020-04-19 21:06:44 -0500

    install react

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './pages/Login.jsx';
+
+ReactDOM.render(<Login />, document.querySelector('body'));
---
+++ src/pages/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    <div>
+    <form action="">
+        <input type="text"/>
+        <input type="text"/>
+        <button type="submit"></button>
+    </form>
+</div>
+}
+
+export default Login;
