4e39cd566da2c114b0e05a10eb01e187b0482ad3
Alex <ajf@fastmail.com>
2020-04-25 22:55:06 -0500

    add mockup content

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -7,12 +7,24 @@ class BookCard extends React.Component {
     return (
       <div className="book-card">
         <section className="details">
-          <h2>Book title</h2>
-          <p>Author</p>
-
+          <h2>El triunfo de las ciudades</h2>
+          <p>Edward Glaeser</p>
+          <p>Taurus</p>
+          <p><span>Inicio: 07/01/2020</span><br /><span>Final: 02/04/2020</span></p>
+          <ul>
+            <li>ciudades</li>
+            <li>desarrollo urbano</li>
+            <li>globalización</li>
+            <li>urbanismo</li>
+            <li>economía</li>
+          </ul>
         </section>
         <section className="highlights">
-
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti iste neque at tempora a sed magnam earum molestias ipsam explicabo dicta placeat sint nostrum odio exercitationem laudantium, odit fugit itaque!Iusto similique unde consequatur? Mollitia ea tempore est excepturi nihil maiores consequatur facere obcaecati iure! Nihil, nesciunt sunt! Ratione voluptatibus delectus quis. Nobis dolores sapiente laborum aspernatur! Doloribus, ipsa laboriosam.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio beatae vel aut voluptas dicta tenetur aspernatur numquam, maxime aperiam quia? Quasi cupiditate deleniti doloribus sapiente placeat, facere pariatur vero nobis.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aperiam obcaecati suscipit quae qui, itaque cumque dolore debitis tenetur magnam dolor error fugiat rem illum voluptas, architecto quisquam distinctio consequuntur?</li>
+          </ul>
         </section>
         <img src={bookCover} alt=""/>
       </div>
