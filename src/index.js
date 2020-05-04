9422788fa00f11684c53a8d460adbbaae88e66f5
Alex <ajf@fastmail.com>
2020-05-03 21:11:45 -0500

    add 4 books to initialState

--- src/index.js
+++ src/index.js
@@ -11,7 +11,35 @@ const initialState = {
       "author": "Peter Thiel",
       "Format": "audiobook",
       "cover": "url",
-    }
+    },
+    {
+      "id" : 2,
+      "title": "El triunfo de las ciudades",
+      "author": "Edward Glaeser",
+      "Format": "paperback",
+      "cover": "url",
+    },
+    {
+      "id" : 3,
+      "title": "Flourish",
+      "author": "Martin E. P. Seligman",
+      "Format": "audiobook",
+      "cover": "url",
+    },
+    {
+      "id" : 4,
+      "title": "Shoe Dog",
+      "author": "Phil Knight",
+      "Format": "audiobook",
+      "cover": "url",
+    },
+    {
+      "id" : 5,
+      "title": "Learning How to Learn",
+      "author": "Barbara Oakley",
+      "Format": "ebook",
+      "cover": "url",
+    },
   ]
 }
 
