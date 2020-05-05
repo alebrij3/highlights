8cd69d3c01d74711b397dd1b6010d738a10815fc
Alex <ajf@fastmail.com>
2020-05-05 16:59:15 -0500

    change bookcard component name to sourcecard

--- src/components/SourceCard.jsx
+++ src/components/SourceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../assets/styles/book-card.styl';
 import bookCover from '../../assets/img/portadas/el-triunfo-de-las-ciudades.jpg';
 
-class BookCard extends React.Component {
+class SourceCard extends React.Component {
   render() {
     return (
       <>
@@ -26,4 +26,4 @@ class BookCard extends React.Component {
   }
 }
 
-export default BookCard;
+export default SourceCard;
