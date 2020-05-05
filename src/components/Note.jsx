e43afff22e1527e2a515a89a434511f6c76afc1b
Alex <ajf@fastmail.com>
2020-05-05 17:41:30 -0500

    remove note dummy text from Note component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ class Note extends React.Component {
   render() {
     return (
       <section className="highlights">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti iste neque at tempora a sed magnam earum molestias ipsam explicabo dicta placeat sint nostrum odio exercitationem laudantium, odit fugit itaque!Iusto similique unde consequatur? Mollitia ea tempore est excepturi nihil maiores consequatur facere obcaecati iure! Nihil, nesciunt sunt! Ratione voluptatibus delectus quis. Nobis dolores sapiente laborum aspernatur! Doloribus, ipsa laboriosam.</p>
+        <p></p>
       </section>
     );
   }
