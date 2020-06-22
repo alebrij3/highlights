ba0057e2708b5b16132797074df3c7f21f5abd3d
Alex <ajf@fastmail.com>
2020-06-22 18:52:01 -0500

    CREATE add-note component

---
+++ src/app/add-note/add-note.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-note',
+  templateUrl: './add-note.component.html',
+  styleUrls: ['./add-note.component.styl']
+})
+export class AddNoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
