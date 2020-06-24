336d98dc47d5248cec8f9589c8a72e38852d5a1e
Alex <ajf@fastmail.com>
2020-06-23 21:07:56 -0500

    ng g c add-book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddNoteComponent } from './add-note/add-note.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddNoteComponent
+    AddNoteComponent,
+    AddBookComponent
   ],
   imports: [
     BrowserModule,
